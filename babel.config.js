77121f3319bf044a36bbf27da5840b9c361738e2
BioPulse Developer <your-email@example.com>
2025-09-16 23:50:12 -0600

    test(config): RN preset + Flow strip; jest transforms; caches cleared

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,5 @@
-﻿module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    ['@babel/preset-typescript', { isTSX: true, allExtensions: true }]
-  ],
-  plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true
-    }]
-  ]
+﻿// babel.config.js
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ['@babel/plugin-transform-flow-strip-types'],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
-module.exports = { 
-  preset: 'react-native', 
-  testEnvironment: 'jsdom', 
-  transform: { 
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest' 
-  }, 
-  transformIgnorePatterns: [ 
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community|react-clone-referenced-element|@testing-library/jest-native|@testing-library/react-native|react-native-gesture-handler)/)' 
-  ], 
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'], 
-  moduleNameMapper: { 
-    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js' 
-  } 
+// jest.config.js
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'jsdom',
+  transform: { '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest' },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native'
+      + '|@react-native'
+      + '|react-native-.*'
+      + '|@react-native-.*'
+      + '|@react-navigation'
+      + ')/)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
